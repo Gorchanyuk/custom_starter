16f5b021fd5ebb4c74436a71f8dcc7f5f4e7b36a
Gorchanyuk <gorchanyk@gmail.com>
2024-05-20 15:32:10 +0400

    Добавил тз

--- logger-http-spring-boot-starter/src/main/java/ru/gorchanyuk/loggerhttpspringbootstarter/config/WebConfig.java
+++ logger-http-spring-boot-starter/src/main/java/ru/gorchanyuk/loggerhttpspringbootstarter/config/WebConfig.java
@@ -1,19 +1,33 @@
 package ru.gorchanyuk.loggerhttpspringbootstarter.config;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
+/**
+ * Отвкчает за настройку конфигурации Spring MVC
+ */
+@Slf4j
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
 
     private final List<HandlerInterceptor> loggingInterceptors;
 
+    /**
+     * Добавление списка пользовательских интерцепторов в конфигурацию Spring MVC
+     * @param registry список Interceptor'ов
+     */
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
+        if (ObjectUtils.isEmpty(loggingInterceptors)){
+            return;
+        }
         loggingInterceptors.forEach(registry::addInterceptor);
+        log.info("Добавление Interceptor'ов для логирования http запросов");
     }
 }
